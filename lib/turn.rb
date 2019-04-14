b6def38f129f7fea0eeff20d2930a7a684cae310
Aicha D <aicha1diallo@gmail.com>
2019-04-14 18:10:42 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+#display_board
+
+ # Should accept a board as an argument and print out the current state of the board for the user.
+
+def display_board(board)
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+  
+end 
+ 
+#valid_move?
+#position_taken
+#move
+require_relative
